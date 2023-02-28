4c840bdff0a733018557ebc530bd7a1f6df0ada2
Brusk Awat <broosk.edogawa@gmail.com>
2023-03-01 00:43:11 +0300

    Adds singleton connection class

---
+++ lib/outboxable/connection.rb
@@ -0,0 +1,26 @@
+require 'singleton'
+
+module Outboxable
+  class Connection
+    include ::Singleton
+    attr_reader :connection
+
+    def initialize
+      @connection = Bunny.new(
+        host: RabbitCarrots.configuration.rabbitmq_host,
+        port: RabbitCarrots.configuration.rabbitmq_port,
+        user: RabbitCarrots.configuration.rabbitmq_user,
+        password: RabbitCarrots.configuration.rabbitmq_password,
+        vhost: RabbitCarrots.configuration.rabbitmq_vhost
+      )
+
+      @connection.start
+    end
+
+    def channel
+      @channel ||= ConnectionPool.new do
+        connection.create_channel
+      end
+    end
+  end
+end
