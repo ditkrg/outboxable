2647c47b53596d57629394f5a6d4d85d0622aec9
Brusk Awat <broosk.edogawa@gmail.com>
2023-04-05 23:52:00 +0300

    Adds mechanism to avoid too many retries

--- lib/outboxable/polling_publisher_worker.rb
+++ lib/outboxable/polling_publisher_worker.rb
@@ -4,8 +4,10 @@ module Outboxable
     sidekiq_options queue: 'critical'
 
     def perform
-      Outbox.pending.find_in_batches(batch_size: 100).each do |batch|
+      Outbox.pending.where(last_attempted_at: [..Time.zone.now, nil]).find_in_batches(batch_size: 100).each do |batch|
         batch.each do |outbox|
+          # This is to prevent a job from being retried too many times. Worst-case scenario is 1 minute delay in jobs. 
+          outbox.update(last_attempted_at: 1.minute.from_now)
           Outboxable::Worker.perform_async(outbox.id)
         end
       end
