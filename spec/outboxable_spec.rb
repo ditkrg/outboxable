7bf316e3097cfd2c87c9564ee149a83aa47ec04a
Brusk Awat <broosk.edogawa@gmail.com>
2023-04-05 23:51:27 +0300

    Adds basic specs

--- spec/outboxable_spec.rb
+++ spec/outboxable_spec.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
+require 'sidekiq/testing' 
 
 RSpec.describe Outboxable do
   it "has a version number" do
     expect(Outboxable::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  context 'polling publisher sidekiq worker' do
+    it "should be able to perform" do
+      expect {
+        Outboxable::PollingPublisherWorker.perform_async
+      }.to change(Outboxable::PollingPublisherWorker.jobs, :size).by(1)
+    end
   end
 end
