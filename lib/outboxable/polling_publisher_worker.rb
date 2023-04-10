286d11984bec2a29608d5e19081dd9b555ca14b5
Brusk Awat <broosk.edogawa@gmail.com>
2023-04-10 10:31:50 +0300

    Adds transitionary state

--- lib/outboxable/polling_publisher_worker.rb
+++ lib/outboxable/polling_publisher_worker.rb
@@ -7,8 +7,8 @@ module Outboxable
       Outbox.pending.where(last_attempted_at: [..Time.zone.now, nil]).find_in_batches(batch_size: 100).each do |batch|
         batch.each do |outbox|
           # This is to prevent a job from being retried too many times. Worst-case scenario is 1 minute delay in jobs.
-          outbox.update(last_attempted_at: 1.minute.from_now)
           Outboxable::Worker.perform_async(outbox.id)
+          outbox.update(last_attempted_at: 1.minute.from_now, status: :processing)
         end
       end
     end
